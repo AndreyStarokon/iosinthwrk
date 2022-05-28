a3bbb4c6b28d81e013ab79478dc50fdeaa86ad31
AndreyStarokon <starokon1990@gmail.com>
2022-05-28 18:20:31 +0300

    123

--- StorageService/StorageService.h
+++ StorageService/StorageService.h
@@ -13,6 +13,6 @@ FOUNDATION_EXPORT double StorageServiceVersionNumber;
 //! Project version string for StorageService.
 FOUNDATION_EXPORT const unsigned char StorageServiceVersionString[];
 
-// In this header, you should import all the public headers of your framework using statements like #import <StorageService/PublicHeader.h>
+
 
 
--- StorageService/StuctPost.swift
+++ StorageService/StuctPost.swift
@@ -18,7 +18,7 @@ public struct Post {
         post.append(Post(author: "Jhon", description: "биток подрос", image: UIImage(named: "bitkoin")!, likes: 123, views: 1145))
         post.append(Post(author: "Виталик", description: "цена достигла 3500", image: UIImage(named: "etherium")!, likes: 723, views: 145))
         post.append(Post(author: "Тетя Глаша", description: "В гречке содержится много витаминов и полезных минералов: В1, В2, В6, фолиевая кислота, кальций, магний, железо, калий. К положительным свойствам относится и то, что эта крупа понижает уровень холестерина в крови, чистит печень от токсинов, обеспечивает правильную работу кишечника, улучшает процесс кроветворения и, как ни странно, борется с бессонницей».", image: UIImage(named: "гречка")!, likes: 44443, views: 9999))
-        post.append(Post(author: "Robert", description: "Buy PIR detector", image: UIImage(named: "123")!, likes: 23, views: 45))
+        post.append(Post(author: "obert", description: "Buy PIR detector", image: UIImage(named: "123")!, likes: 23, views: 45))
         
         return post
     }
