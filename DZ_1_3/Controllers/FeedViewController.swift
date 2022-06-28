d617c66d8a56349d5e158ea2e26f13ac89ce9b7e
AndreyStarokon <starokon1990@gmail.com>
2022-06-28 20:36:51 +0300

    корректировка дз

--- DZ_1_3/Controllers/FeedViewController.swift
+++ DZ_1_3/Controllers/FeedViewController.swift
@@ -21,61 +21,109 @@ class FeedViewController: UIViewController {
         
         return stack
     }()
-    private let buttonOne: UIButton = {
-        let button = UIButton()
-        button.setTitle("Показать пост", for: .normal)
-        button.backgroundColor = .systemBlue
-        button.layer.cornerRadius = 16
-        button.layer.shadowOffset = .init(width: 4, height: 4)
-        button.layer.shadowOpacity = 1.0;
-        button.layer.shadowRadius = 4;
-        button.setTitleColor(.black, for: .normal)
+    private lazy var word: UITextField = {
+        let word = UITextField()
+        word.backgroundColor = .white
+        word.layer.cornerRadius = 12
+        word.layer.borderWidth = 1
+        word.layer.borderColor = UIColor.black.cgColor
+        word.textColor = .black
+        word.leftViewMode = .always
+        word.clearButtonMode = .always
+        word.returnKeyType = .done
+        word.placeholder = " "
+        word.font = UIFont(name: "Apple SD Ghotic Neo", size: 15)
+        word.tintColor = .systemBlue
         
-        return button
+        word.autocapitalizationType = .none
+        word.translatesAutoresizingMaskIntoConstraints = false
+        return word
     }()
-    private let buttonTwo: UIButton = {
-        let button = UIButton()
-        button.setTitle("Показать пост", for: .normal)
-        button.backgroundColor = .systemBlue
-        button.layer.cornerRadius = 16
-        button.layer.shadowOffset = .init(width: 4, height: 4)
-        button.layer.shadowOpacity = 1.0;
-        button.layer.shadowRadius = 4;
-        button.setTitleColor(.black, for: .normal)
-        
-        return button
-    }()
+
+    private lazy var wordCheck = CustomButton(title: "проверка пароля", color: UIColor(named: "MainColor") ?? .systemBlue, colorTitle: .white, borderWidth: 0, cornerRadius: 10) {
+            if self .word.text != ""{
+                self.modelCheck.check(inputWord: self.word.text!)
+            } else {
+                self.word.placeholder = "Введите пароль"
+            }
+        }
+    private lazy var buttonOne = CustomButton(title: "показать пост 1", color: .systemBlue, colorTitle: .black, borderWidth: 1, cornerRadius: 10){ self.navigationController?.pushViewController(PostViewController(), animated: true)}
+    private lazy var buttonTwo = CustomButton(title: "показать пост 2", color: .systemBlue, colorTitle: .black, borderWidth: 1, cornerRadius: 10){ self.navigationController?.pushViewController(PostViewController(), animated: true)}
+    
+    
+    private let modelCheck: ModelCheck
+        init (modelCheck: ModelCheck) {
+            self.modelCheck = modelCheck
+            super.init(nibName: nil, bundle: nil)
+        }
+        required init?(coder: NSCoder) {
+            fatalError("init(coder:) has not been implemented")
+        }
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .white
         title = "Лента"
-       setupLayer()
-        buttonOne.addTarget(self, action: #selector(buttonShowPost), for: .touchUpInside)
-        buttonTwo.addTarget(self, action: #selector(buttonShowPost), for: .touchUpInside)
+        setupLayer()
+        NotificationCenter.default.addObserver(self,selector:#selector(redLable),name:Notification.Name.redLable,object: nil)
+        NotificationCenter.default.addObserver(self,selector:#selector(greenLable),name:Notification.Name.greenLable, object: nil)
+            }
 
-    }
+            @objc func redLable() {
+                word.backgroundColor = .red
+            }
+            @objc func greenLable() {
+                word.backgroundColor = .green
+            }
+
     override func viewDidLayoutSubviews() {
         super.viewDidLayoutSubviews()
        
     }
+    
     private func setupLayer() {
         view.addSubview(stackView)
+        view.addSubview(word)
+        view.addSubview(wordCheck!)
+        
         NSLayoutConstraint.activate([
-            stackView.centerXAnchor.constraint(equalTo: view.centerXAnchor),
-            stackView.centerYAnchor.constraint(equalTo: view.centerYAnchor),
-            stackView.heightAnchor.constraint(equalToConstant: 150),
-            stackView.widthAnchor.constraint(equalToConstant: 300)
+        stackView.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+        stackView.centerYAnchor.constraint(equalTo: view.centerYAnchor),
+        stackView.heightAnchor.constraint(equalToConstant: 150),
+        stackView.widthAnchor.constraint(equalToConstant: 300)
         ])
-        stackView.addArrangedSubview(buttonOne)
-        stackView.addArrangedSubview(buttonTwo)
+        NSLayoutConstraint.activate([
+        word.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 10),
+        word.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -10),
+        word.topAnchor.constraint(equalTo: stackView.bottomAnchor, constant: 15),
+        word.heightAnchor.constraint(equalToConstant: 40)
+        ])
+        NSLayoutConstraint.activate([
+        wordCheck!.topAnchor.constraint(equalTo: word.bottomAnchor, constant: 30),
+        wordCheck!.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 50),
+        wordCheck!.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -50),
+        wordCheck!.heightAnchor.constraint(equalToConstant: 50)
+                ])
+        stackView.addArrangedSubview(buttonOne!)
+        stackView.addArrangedSubview(buttonTwo!)
     }
-    @objc  func buttonShowPost() {
-        let pcView = PostViewController()
-        pcView.view.backgroundColor = .yellow
-        pcView.title = "пост"
-        navigationController?.pushViewController(pcView, animated: true)
-        //создание контроллера
+
+}
+
+extension FeedViewController: UITextFieldDelegate {
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        view.endEditing(true)
+        if self .word.text != ""{
+            self.modelCheck.check(inputWord: self.word.text!)
+        } else {
+            self.word.placeholder = "Введите слово"
+        }
+        return true
     }
 }
 
+public extension NSNotification.Name {
+    static let redLable = NSNotification.Name("redLable")
+    static let greenLable = NSNotification.Name("greenLable")
+}
