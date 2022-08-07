e62179da6864ec1027220df651a9b0c19395d75b
AndreyStarokon <starokon1990@gmail.com>
2022-08-07 18:45:15 +0300

    realm

--- DZ_1_3/NetworkServises/NetworkManager.swift
+++ DZ_1_3/NetworkServises/NetworkManager.swift
@@ -10,27 +10,25 @@ import Foundation
 
 
 struct NetworkManager {
-    
-        private static let session = URLSession.shared
-            
-            static func dataTask(url: URL) {
-                let task = session.dataTask(with: url) {data, response, error in
-                    
-                    guard error == nil else {
-                        print(error.debugDescription)
-                        return
-                    }
-                    
-                    guard let httpResponse = response as? HTTPURLResponse, httpResponse.statusCode == 200 else { return }
-                    
-                    print("allHeadersFields: \(httpResponse.allHeaderFields as! [String: Any])")
-                    print("statusCode: \(httpResponse.statusCode)")
-                    print("data: \(String(data: data!, encoding: .utf8))")
+
+    private static let session = URLSession.shared
+
+        static func dataTask(url: URL) {
+            let task = session.dataTask(with: url) {data, response, error in
+
+                guard error == nil else {
+                    print(error.debugDescription)
+                    return
                 }
-                
-                task.resume()
+
+                guard let httpResponse = response as? HTTPURLResponse, httpResponse.statusCode == 200 else { return }
+
             }
-    static func infoDataTask(url: URL, block: @escaping (Data) -> Void) {
+
+            task.resume()
+        }
+
+        static func infoDataTask(url: URL, block: @escaping (Data) -> Void) {
                 let task = session.dataTask(with: url) { data, response, error in
                     print("check")
 
