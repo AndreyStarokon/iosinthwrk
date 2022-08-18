85e18078cf7478026c5288c3b5e74c02cbdc8471
AndreyStarokon <starokon1990@gmail.com>
2022-08-18 22:23:31 +0300

    coreData

---
+++ DZ_1_3/CoreData/CoreDataStack.swift
@@ -0,0 +1,8 @@
+//
+//  CoreDataStack.swift
+//  VK_app
+//
+//  Created by Ffhh Qerg on 16.08.2022.
+//
+
+import Foundation
---
+++ DZ_1_3/CoreData/Task.swift
@@ -0,0 +1,8 @@
+//
+//  Task.swift
+//  VK_app
+//
+//  Created by Ffhh Qerg on 16.08.2022.
+//
+
+import Foundation
